735a97e2d72d6c2f170f650dbdf3caa17ea4e03b
Jandro97 <alejandreitor97@gmail.com>
2018-01-18 16:53:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/clase/dogypark/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.clase.dogypark;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv_registrar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_registrar=findViewById(R.id.tv_reg);
+        tv_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentReg= new Intent(MainActivity.this, Registro.class);
+                MainActivity.this.startActivity(intentReg);
+            }
+        });
+    }
+}

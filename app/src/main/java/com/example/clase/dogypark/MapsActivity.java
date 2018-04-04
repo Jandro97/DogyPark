0df024f204e74dfd9560f041b21ce515148f4a34
Jandro97 <alejandreitor97@gmail.com>
2018-04-04 11:03:17 +0200

    localizacion y mapa funcionando

--- app/src/main/java/com/example/clase/dogypark/MapsActivity.java
+++ app/src/main/java/com/example/clase/dogypark/MapsActivity.java
@@ -1,42 +1,58 @@
 package com.example.clase.dogypark;
 
-import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.Manifest;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     MapView mapView;
-
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    FusedLocationProviderClient mFusedLocationProviderClient;
+    private Location mLastKnownLocation;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView= inflater.inflate(R.layout.maplayout, container, false);
+        View rootView = inflater.inflate(R.layout.maplayout, container, false);
 
-        mapView=(MapView) rootView.findViewById(R.id.map);
+
+        mapView = (MapView) rootView.findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
         mapView.onResume();
+        Log.d("MAP", "Entrando en onCreateView");
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
 
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         mapView.getMapAsync(this);
@@ -54,22 +70,136 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback {
     }
 
 */
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        Log.d("MAP", "Entrando en onMapReady");
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap = googleMap;
+        int n = 0;
+        GoogleMapOptions options = new GoogleMapOptions().liteMode(true);
+
+        /*mMap.setMinZoomPreference(15.0f);
+        mMap.setMaxZoomPreference(18.0f);*/
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        checkLocationPermission();
+
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marcador"));
+        /*if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            Toast.makeText(getContext(),"No se detecta el GPS", Toast.LENGTH_SHORT).show();
+        }*/
+       /* mMap.moveCamera(CameraUpdateFactory.newLatLng());*/
+    }
+
+    public boolean checkLocationPermission() {
+        Log.d("MAP", "Entrando en CheckLocationPermission");
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Log.d("MAP", "Explicando Permisos");
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(getContext())
+                        .setTitle(R.string.title_location_permission)
+                        .setMessage("Esta aplicacion necesita los permisos de localizacion")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(getActivity(),
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                Log.d("MAP", "Pidiendo Permisos");
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            Log.d("MAP", "Tiene Permisos");
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            getDeviceLocation();
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        Log.d("MAP", "Entrando en onRequestPermission");
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(getContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        mMap.setMyLocationEnabled(true);
+                        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                        getDeviceLocation();
+                        Toast.makeText(getContext(), "Permiso concedido", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+
+                    Toast.makeText(getContext(), "permiso denegado", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+        }
+    }
+
+    private void getDeviceLocation() {
+        Log.d("deviceloc", "entrando en getDeviceLocation");
+        try {
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED) {
+                Task locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(getActivity(), new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mLastKnownLocation = (Location) task.getResult();
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), 18));
+                            Log.d("deviceloc", "cargando posicion:"+mLastKnownLocation.getLatitude()+", "+mLastKnownLocation.getLongitude());
+                        } else {
+                            Log.d("deviceloc", "Current location is null. Using defaults.");
+                            Log.e("deviceloc", "Exception: %s", task.getException());
+                            LatLng latLng= new LatLng(0,0);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        }catch(SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
     }
 }
--- app/src/main/java/com/example/clase/dogypark/mapfragment.java
+++ app/src/main/java/com/example/clase/dogypark/mapfragment.java
@@ -9,21 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link mapfragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link mapfragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class mapfragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -33,15 +24,8 @@ public class mapfragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment mapfragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
+
     public static mapfragment newInstance(String param1, String param2) {
         mapfragment fragment = new mapfragment();
         Bundle args = new Bundle();
@@ -67,7 +51,7 @@ public class mapfragment extends Fragment {
         return inflater.inflate(R.layout.fragment_mapfragment, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
